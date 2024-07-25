5673b87fbb8b4802ffe3f3c470e81172a99611e1
celery1116 <celery1116@gmail.com>
2024-07-25 18:18:58 +0800

    202407251818

--- products.py
+++ products.py
@@ -1,4 +1,5 @@
 products = []
+
 while True:
 	name = input('請輸入商品名稱')
 	if name == 'q' :
@@ -8,3 +9,9 @@ while True:
 
 for p in products:
 	print(p[0],'的價格是',p[1])
+
+file = 'products.csv'
+with open (file,'w') as f:
+	f.write('品項'+ ',' + '價格' + '\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
